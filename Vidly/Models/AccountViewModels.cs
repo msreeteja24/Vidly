b2afe42c0aa719a54bd4525d41f3c67483b38629
msreeteja24 <msreeteja24@gmail.com>
2025-06-26 20:19:38 +0530

    Adding Profile Data - Adding Driving License to the user

--- Vidly/Models/AccountViewModels.cs
+++ Vidly/Models/AccountViewModels.cs
@@ -65,9 +65,15 @@ namespace Vidly.Models
     public class RegisterViewModel
     {
         [Required]
+        [StringLength(255)]
+        public string DrivingLicense { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
