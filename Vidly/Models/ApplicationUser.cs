98bd304f5c4ec0df23291a0e6e239c282c62906f
msreeteja24 <msreeteja24@gmail.com>
2025-06-27 20:09:46 +0530

    Building an API for renting movies

--- Vidly/Models/ApplicationUser.cs
+++ Vidly/Models/ApplicationUser.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.ComponentModel.DataAnnotations;
-using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -27,26 +26,5 @@ namespace Vidly.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
 
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Movie> Movies { get; set; }
-        public DbSet<MembershipType> MembershipTypes { get; set; }
-        public DbSet<Genre> Genres { get; set; }
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-
-
-
-
-    }
 }
